c95d84a3fa75736a5dd1b6164650b133cdbd5bcf
Thomas Conner <thomas@kinvey.com>
2017-02-24 10:17:37 -0500

    Remove dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _ngKinvey = require('./ngKinvey');
-
-var _ngKinvey2 = _interopRequireDefault(_ngKinvey);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = _ngKinvey2.default;
